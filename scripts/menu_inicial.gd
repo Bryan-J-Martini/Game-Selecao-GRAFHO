c099aff0a7575f662b1aa2cfa7067b22528a49cd
Lorenzo Conti <lorenzo.peripatoconti123@gmail.com>
2026-09-24 23:43:39 -0300

    Update menu_inicial.gd

--- scripts/menu_inicial.gd
+++ scripts/menu_inicial.gd
@@ -3,6 +3,8 @@ extends Control
 func _on_jogar_pressed() -> void:
 	get_tree().change_scene_to_file("res://cenas/mundo.tscn")
 
+func _on_configuracoes_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/configuracoes.tscn")
 
 func _on_sair_pressed() -> void:
 	get_tree().quit()
